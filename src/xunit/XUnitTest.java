60be3a226c26317ea7b815b54a2157826c79ff00
bongdong2 <jsu373712@naver.com>
2020-03-11 09:41:46 +0900

    second commit

--- src/xunit/XUnitTest.java
+++ src/xunit/XUnitTest.java
@@ -2,9 +2,6 @@ package xunit;
 
 public class XUnitTest {
     public static void main(String[] args) {
-        WasRun wasRun = new WasRun();
-        System.out.println(wasRun.wasRun); // false
-        wasRun.testMethod();
-        System.out.println(wasRun.wasRun); // true
+        new TestCaseTest("testRunning").run();
     }
 }
